611f5c26a339fc00859cdb527433aebea9e8d3fd
Tobias K <nox@raynode.de>
2020-06-22 09:22:19 +0200

    rewrite to handle typescript

---
+++ rollup.config.js
@@ -0,0 +1,29 @@
+import typescript from "rollup-plugin-typescript2";
+import pkg from "./package.json";
+
+export default {
+  input: "src/index.ts",
+  output: [
+    {
+      file: pkg.main,
+      format: "cjs",
+      sourcemap: true,
+    },
+    {
+      file: pkg.module,
+      format: "es",
+      sourcemap: true,
+    },
+  ],
+  // adding this to remove error for: https://rollupjs.org/guide/en/#warning-treating-module-as-external-dependency
+  external: [
+    ...Object.keys(pkg.dependencies || {}),
+    ...Object.keys(pkg.peerDependencies || {}),
+  ],
+  plugins: [
+    typescript({
+      typescript: require("typescript"),
+      tsconfig: "./tsconfig.build.json",
+    }),
+  ],
+};
