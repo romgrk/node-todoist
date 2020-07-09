624083768610fe7055a1b8d53917c2c6b2f1a070
Tobias K <nox@raynode.de>
2020-07-09 15:00:17 +0200

    updated exports to deliver types

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,10 @@
-import { Todoist as v8 } from './v8'
+import { Todoist as TodoistV8 } from './v8'
+import * as TodoistV8Types from './v8-types'
 
-const Todoist = v8
+// export V8 as the current version
+export const Todoist = TodoistV8
 
-export { Todoist, v8 }
+// create backwards compatibility list
+export const v8 = TodoistV8
+// export Types as well
+export { TodoistV8Types }
