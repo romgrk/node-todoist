8332c89511304c23d4313ef7e6a5ce5aca09cf43
riexn <eriexn@gmail.com>
2021-07-06 07:47:22 +0000

    added tets to create a due date with a strin and with a date options(s)

--- src/v8.test.ts
+++ src/v8.test.ts
@@ -54,8 +54,34 @@ describe('.items due dates', () => {
   })
 
   test('.add() with string due date', async () => {
-    item = await api.items.add({ content: 'testing-task', due: { string: 'tomorrow' } })
+    item = await api.items.add({ content: 'testing-task', due: { string: '17th of july 2021' } })
+    expect(item).toMatchObject({
+      content: 'testing-task',
+      due: {
+        date: '2021-07-17',
+        is_recurring: false,
+        lang: 'en',
+        string: '17th of july 2021',
+        timezone: null,
+      },
+    })
+  })
+
+  test('.add() with due date', async () => {
+    item = await api.items.add({
+      content: 'testing-task',
+      due: { date: '2018-10-14' },
+    })
     console.log(item)
-    expect(item).toMatchObject({ content: 'testing-task' })
+    expect(item).toMatchObject({
+      content: 'testing-task',
+      due: {
+        date: '2018-10-14',
+        is_recurring: false,
+        lang: 'en',
+        string: '2018-10-14',
+        timezone: null,
+      },
+    })
   })
 })
